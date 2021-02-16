dfdd4f3f769b97698cfa2e5845bc7c92ec793a88
Keerthana <keerthana271995@gmail.com>
2021-02-17 00:04:28 +0530

    first commit

---
+++ react-todo-app/src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class TodoContainer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello from TodoContainer</h1>
+                <p> this is from keerthana</p>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer
